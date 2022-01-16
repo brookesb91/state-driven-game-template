e81464afc7ac3c704e16de44ca2559e9feb90425
Ben Brookes <brookesb91@hotmail.com>
2022-01-16 16:21:43 +0000

    :construction: Add environments - fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,49 @@
+const webpack = require('webpack');
 const path = require('path');
 const PathsPlugin = require('tsconfig-paths-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/main.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    plugins: [new PathsPlugin({})],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        'src/index.html',
-        'src/manifest.webmanifest',
-        'src/service-worker.js',
-        { from: 'src/assets/', to: 'assets' },
-        { from: 'src/icons/', to: 'icons' },
+module.exports = (env) => {
+  return {
+    entry: './src/main.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
       ],
-    }),
-  ],
-  watchOptions: {
-    ignored: ['dist/', '/node_modules/'],
-  },
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      plugins: [new PathsPlugin({})],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          'src/index.html',
+          'src/manifest.webmanifest',
+          'src/service-worker.js',
+          { from: 'src/assets/', to: 'assets' },
+          { from: 'src/icons/', to: 'icons' },
+        ],
+      }),
+      ...(env.production
+        ? [
+            new webpack.NormalModuleReplacementPlugin(
+              /src\/env\/environment\/environment\.ts/,
+              'src/env/environment/environment.prod.ts'
+            ),
+          ]
+        : []),
+    ],
+    watchOptions: {
+      ignored: ['dist/', '/node_modules/'],
+    },
+  };
 };
